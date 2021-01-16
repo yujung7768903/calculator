3b8d42a0ab68fbef5bfe0365f20fa0487e3281bd
yujung7768903 <yu-jung31476@naver.com>
2021-01-17 03:18:02 +0900

    배열을 이용한 연산 구현

--- main.js
+++ main.js
@@ -15,60 +15,87 @@ let total;
 let temp;
 let operator = "";
 let progressValue;
+let inputValueArr = [];
+let lastValue = progress.innerText.slice(-1);
 
 
 function print(num) {
-    if (num === '/' || num === '*' || num === '+' || num === '-') {
-        operator = num;
-        progress.innerText = progress.innerText + numberInput.value;
+    if (num === '/' || num === '*' || num === '+' || num === '-' || num === '%') {
+        if (Number(lastValue) == NaN) {
+            progress.innerText = progress.innerText.slice(0,-1) + num;
+            inputValueArr.pop();
+        }
+        else {
+            inputValueArr.push(Number(numberInput.value));
+        }
+        inputValueArr.push(num);
         numberInput.value = null;
+        progress.innerText = progress.innerText + num;
     }
     else if (num === 'del') {
+        progress.innerText = progress.innerText.slice(0,-1);
         numberInput.value = numberInput.value.slice(0,-1);
     }
     else if (num === 'c') {
+        progress.innerText = null;
         numberInput.value = null;
         result.innerText = null;
+        inputValueArr = [];
     }
     else {
-//        temp = numberInput.value;
         numberInput.value = numberInput.value + num;
-//        progressValue = 
-    }    
+        progress.innerText = progress.innerText + num;
+    }
     temp = numberInput.value;
-    console.log(`temp = ${temp}, type = ${typeof temp}`);
 }
+
 totalButton.addEventListener('click', () => {
-    total = numberInput.value;
+    if (Number(lastValue) === NaN) {
+        progress.innerText = progress.innerText.slice(0,-1);
+        inputValueArr.pop();
+    }
+    inputValueArr.push(Number(numberInput.value));
+    console.log(inputValueArr);
+    inputValueArr.forEach(element => {
+        if (element === '/') {
+            let idxDiv = inputValueArr.indexOf('/');
+            let calDiv = inputValueArr.slice(idxDiv-1,idxDiv+2);
+            console.log(calDiv);
+            let insteadDiv = calDiv[0] / calDiv[2];
+            inputValueArr.splice(idxDiv-1, 3, insteadDiv);
+            console.log(inputValueArr);
+
+        }
+        else if (element === '*') {
+            let idxMul = inputValueArr.indexOf('*');      
+            let calMul = inputValueArr.slice(idxMul-1,idxMul+2);
+            console.log(calMul);
+            let insteadMul = calMul[0] * calMul[2];
+            inputValueArr.splice(idxMul-1, 3, insteadMul);
+            console.log(inputValueArr);
+
+        }    
+    });
+    inputValueArr.forEach(element => {
+        console.log(element);
+        if (element === '-') {
+            let idxSub = inputValueArr.indexOf('-');
+            let calSub = inputValueArr.slice(idxSub-1,idxSub+2);
+            console.log(calSub);
+            let insteadSub = calSub[0] - calSub[2];
+            console.log(insteadSub);
+            inputValueArr.splice(idxSub-1, 3, insteadSub);
+            console.log(inputValueArr);
+        }
+    })
+    inputValueArr.forEach(element => {
+        if (element === '+') {
+            let idxPlu = inputValueArr.indexOf('+');
+            inputValueArr.splice(idxPlu,1);
+            console.log(inputValueArr);
+        }
+    })
+    result.innerText = inputValueArr.reduce(function add(sum, currValue){
+        return sum + currValue;
+    }, 0);
 });
-
-let exceptPlusArr = [];
-let plusArr = [];
-let exceptDivideArr = [];
-let exceptMultiply = [];
-
-function caculate(total) {
-    let forCaculateArr = total.split('+');
-    forCaculateArr.array.forEach(element => {
-        if (Number(element)==NaN) { // 나누기, 곱하기, 빼기가 들어있는 요소들을 배열에 저장
-            exceptPlusArr.push(element);
-        }
-        else {
-            plusArr.push(element); //양수인 것은 저장
-        }
-    });
-    exceptPlusArr.array.forEach(element => {
-        for(let i = 0; i < element.length ; i--) {
-            if (element[i] == '/' || element[i] == '*') {
-                if (element[i] == '/') {
-                    exceptDivideArr = element.split('/');
-                }
-                else if (element[i] == '*') {
-                    exceptMultiplyelement.split('*');
-                }
-            }
-        }
-    });
-}
-
-console.log(Number('2+4-3'))
